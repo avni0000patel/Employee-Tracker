5bf2b1514cc6424ec704a1b54d4e3ea0c90375bf
Avni <avni0000patel@gmail.com>
2022-08-21 19:44:02 -0400

    create table role

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,10 @@ CREATE TABLE department (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL, 
+    department_id INT NOT NUll,
+    PRIMARY KEY (id)
+);
