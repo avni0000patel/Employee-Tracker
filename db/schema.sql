b13ecc0e4d26c4c8b72c458c527c97b4f2b93f9d
Avni <avni0000patel@gmail.com>
2022-08-21 19:07:26 -0400

    create table department

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS employeeTracker_db;
 CREATE DATABASE employeeTracker_db;
 
 USE employeeTracker_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
