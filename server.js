a4fa03d53c192f43eb80ddfb2e4551b259f984ba
Avni <avni0000patel@gmail.com>
2022-08-22 17:19:22 -0400

    const db

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'Watermelon-0!',
+      database: 'employeeTracker_db'
+    },
+    console.log(`Connected to the employeeTracker_db database.`)
+);
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
